7e1804c1f98fb922cc93d6c9030f850cf23dffd4
UtsahiAdya <adyautsahi@gmail.com>
2025-04-13 20:04:55 +0530

    Fix navbar.css path issue for Vercel build

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import "./navbar.css";
+import "./Navbar.css";
 
 const Navbar = () => {
   const [user, setUser] = useState(null);
